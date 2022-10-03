9e44a1811bba05a88f03fb27dccfb1d3a1f92290
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2022-10-03 17:28:57 +0100

    test tawk.to

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,11 @@ import "../styles/globals.css";
 import { Provider } from "react-redux";
 import { useStore } from "../store/store";
 import { AppWrapper } from "../context";
-
+import tawkWidjet from "../utils/tawk"
+import { useEffect } from "react";
 export default function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
-
+  useEffect(() => { tawkWidjet(); }, [])
   return (
     <Provider store={store}>
       <SessionProvider session={pageProps.session}>
---
+++ utils/tawk.js
@@ -0,0 +1,9 @@
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+export default function () {
+  var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = 'https://embed.tawk.to/633b05dd54f06e12d898337b/1gef8tqdt';
+  s1.charset = 'UTF-8';
+  s1.setAttribute('crossorigin', '*');
+  s0.parentNode.insertBefore(s1, s0);
+};
