2ef43901c2c98b913f3f7611ad678510f2042af7
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2023-10-17 16:50:31 +0100

    test google search console

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,14 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="theme-color" content="#131325" />
+          <meta
+            name="google-site-verification"
+            content="jGch0f5SusiEDUsTTkNZ0VxZn8-yTFdkxYhKNR5AT-E"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Roboto+Mono&display=swap"
             rel="stylesheet"
           />
-
         </Head>
         <body>
           <Main />
