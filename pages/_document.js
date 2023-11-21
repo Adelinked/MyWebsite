569f729d906bf14f15e8f3c01e3a3e2afaea0da0
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2023-11-21 19:13:23 +0100

    add google search console tag

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="theme-color" content="#131325" />
-          <meta
-            name="google-site-verification"
-            content="jGch0f5SusiEDUsTTkNZ0VxZn8-yTFdkxYhKNR5AT-E"
-          />
+          <meta name="google-site-verification" content="Lael8XNyAk6_v6ZlbaLIIb2L3sa_eE0T8XezFh_2A9Y" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Roboto+Mono&display=swap"
             rel="stylesheet"
